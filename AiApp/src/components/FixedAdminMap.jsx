93e2b0df84c36732d5ff93f190637bfd9cea6846
adoorknob <quantengwai@gmail.com>
2024-10-13 23:16:11 +0800

    change link

--- AiApp/src/components/FixedAdminMap.jsx
+++ AiApp/src/components/FixedAdminMap.jsx
@@ -4,8 +4,8 @@ const LocalHtmlEmbed = () => {
   return (
     <div>
       <iframe 
-        src={`http://localhost:5173/map.html`} // Point to the local HTML file
-        // src={`${process.env.VITE_REACT_APP_firebase_authDomain}/map.html`} // Point to the local HTML file
+        // src={`http://localhost:5173/map.html`} // Point to the local HTML file
+        src={`${process.env.VITE_REACT_APP_firebase_authDomain}/map.html`} // Point to the local HTML file
         width="1200" 
         height="500" 
         title="Embedded Local HTML"
--- AiApp/src/components/FixedShipMap.jsx
+++ AiApp/src/components/FixedShipMap.jsx
@@ -4,8 +4,8 @@ const FixedShipMap = () => {
   return (
     <div>
       <iframe 
-        src={`http://localhost:5173/longest_path_map.html`} // Point to the local HTML file
-        // src={`${process.env.VITE_REACT_APP_firebase_authDomain}/map.html`} // Point to the local HTML file
+        // src={`http://localhost:5173/longest_path_map.html`} // Point to the local HTML file
+        src={`${process.env.VITE_REACT_APP_firebase_authDomain}/map.html`} // Point to the local HTML file
         width="1177" 
         height="500" 
         title="Embedded Local HTML"
